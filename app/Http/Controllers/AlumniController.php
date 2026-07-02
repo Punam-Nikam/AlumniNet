1b6c027bc94198b17aa3bb5e0ef6fbb2e102ea47
Punam-Nikam <punamnikam.835@gmail.com>
2026-07-02 16:52:11 +0530

    Task 1-Design HTML5 alumni directory with profile cards and search - Completed

--- app/Http/Controllers/AlumniController.php
+++ app/Http/Controllers/AlumniController.php
@@ -7,6 +7,7 @@ class AlumniController extends Controller
     public function index()
     {
         // Dummy alumni data for now (real DB data comes in Task 4-7)
+        //task 1 here
         $alumni = [
             [
                 'name'    => 'Rahul Sharma',
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+//task 1
 use App\Http\Controllers\AlumniController;
 
 Route::get('/alumni', [AlumniController::class, 'index']);
